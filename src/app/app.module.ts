83c18b8c9dff8f6cccc40a3e6689fc490209ff95
Gabriel Trossero <gabi_trossero@hotmail.com>
2022-08-08 08:21:21 -0300

    register Locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import localeEs from '@angular/common/locales/es'; //módulo para traducciones
+import { registerLocaleData } from '@angular/common'; //para aplicar las traducciones
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+registerLocaleData(localeEs); //debe cargar antes que todo
 
 @NgModule({
   declarations: [
@@ -12,7 +16,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID, useValue: 'es-Es'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
