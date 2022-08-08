4e4c99bff00cdeda1bf628f6cb9ad0fbc0a514da
Gabriel Trossero <gabi_trossero@hotmail.com>
2022-08-08 09:27:35 -0300

    CurrencyPipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pipes';
+
+  //CurrentcyPipe (divisa)
+  precio: number;
+
+  constructor() {
+    this.precio = 189.99;
+  }
+
 }
